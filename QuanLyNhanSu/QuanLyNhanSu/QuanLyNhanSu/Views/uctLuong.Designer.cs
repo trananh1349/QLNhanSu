034ec16bfda32b0ee409d8b4104ce3a33f77bbb2
thanhtung0310 <thanhtung031099@gmail.com>
2020-03-18 18:07:48 +0700

    Design frmLuong
    Mới thiết kế giao diện.
    Chưa thao tác với CSDL.

--- QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/Views/uctLuong.Designer.cs
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/Views/uctLuong.Designer.cs
@@ -28,10 +28,361 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.gboxAdd = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbWageAdd = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.cbbCateAdd = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbDayAdd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelAdd = new System.Windows.Forms.Label();
+            this.dataGridViewWage = new System.Windows.Forms.DataGridView();
+            this.label13 = new System.Windows.Forms.Label();
+            this.MaLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgayCong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LuongCB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gBoxDelete = new System.Windows.Forms.GroupBox();
+            this.labelEdit = new System.Windows.Forms.Label();
+            this.tbDayEdit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.cbbCateEdit = new System.Windows.Forms.ComboBox();
+            this.tbWageEdit = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.gboxAdd.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWage)).BeginInit();
+            this.gBoxDelete.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gboxAdd
+            // 
+            this.gboxAdd.BackColor = System.Drawing.Color.White;
+            this.gboxAdd.Controls.Add(this.labelAdd);
+            this.gboxAdd.Controls.Add(this.tbDayAdd);
+            this.gboxAdd.Controls.Add(this.label1);
+            this.gboxAdd.Controls.Add(this.buttonAdd);
+            this.gboxAdd.Controls.Add(this.cbbCateAdd);
+            this.gboxAdd.Controls.Add(this.tbWageAdd);
+            this.gboxAdd.Controls.Add(this.label7);
+            this.gboxAdd.Controls.Add(this.label6);
+            this.gboxAdd.Location = new System.Drawing.Point(21, 14);
+            this.gboxAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gboxAdd.Name = "gboxAdd";
+            this.gboxAdd.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gboxAdd.Size = new System.Drawing.Size(634, 340);
+            this.gboxAdd.TabIndex = 8;
+            this.gboxAdd.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(23, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(229, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Lương cơ bản:";
+            // 
+            // tbWageAdd
+            // 
+            this.tbWageAdd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbWageAdd.Location = new System.Drawing.Point(274, 204);
+            this.tbWageAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbWageAdd.Multiline = true;
+            this.tbWageAdd.Name = "tbWageAdd";
+            this.tbWageAdd.Size = new System.Drawing.Size(329, 36);
+            this.tbWageAdd.TabIndex = 13;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.MidnightBlue;
+            this.buttonAdd.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.ForeColor = System.Drawing.Color.White;
+            this.buttonAdd.Location = new System.Drawing.Point(175, 274);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(251, 48);
+            this.buttonAdd.TabIndex = 18;
+            this.buttonAdd.Text = "Thêm mới";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // cbbCateAdd
+            // 
+            this.cbbCateAdd.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbCateAdd.FormattingEnabled = true;
+            this.cbbCateAdd.Items.AddRange(new object[] {
+            "Hop dong lao dong",
+            "Hop dong CTV"});
+            this.cbbCateAdd.Location = new System.Drawing.Point(274, 101);
+            this.cbbCateAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbbCateAdd.Name = "cbbCateAdd";
+            this.cbbCateAdd.Size = new System.Drawing.Size(329, 35);
+            this.cbbCateAdd.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(23, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Loại HDLD: ";
+            // 
+            // tbDayAdd
+            // 
+            this.tbDayAdd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDayAdd.Location = new System.Drawing.Point(274, 153);
+            this.tbDayAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbDayAdd.Multiline = true;
+            this.tbDayAdd.Name = "tbDayAdd";
+            this.tbDayAdd.Size = new System.Drawing.Size(329, 36);
+            this.tbDayAdd.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(23, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 32);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Ngày công:";
+            // 
+            // labelAdd
+            // 
+            this.labelAdd.AutoSize = true;
+            this.labelAdd.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold);
+            this.labelAdd.Location = new System.Drawing.Point(134, 20);
+            this.labelAdd.Name = "labelAdd";
+            this.labelAdd.Size = new System.Drawing.Size(327, 41);
+            this.labelAdd.TabIndex = 21;
+            this.labelAdd.Text = "Thêm mới lương";
+            this.labelAdd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridViewWage
+            // 
+            this.dataGridViewWage.AllowUserToAddRows = false;
+            this.dataGridViewWage.AllowUserToDeleteRows = false;
+            this.dataGridViewWage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewWage.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaLuong,
+            this.NgayCong,
+            this.LuongCB});
+            this.dataGridViewWage.Location = new System.Drawing.Point(340, 409);
+            this.dataGridViewWage.Name = "dataGridViewWage";
+            this.dataGridViewWage.ReadOnly = true;
+            this.dataGridViewWage.RowHeadersWidth = 51;
+            this.dataGridViewWage.RowTemplate.Height = 24;
+            this.dataGridViewWage.Size = new System.Drawing.Size(725, 132);
+            this.dataGridViewWage.TabIndex = 34;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Bold);
+            this.label13.Location = new System.Drawing.Point(578, 380);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(229, 26);
+            this.label13.TabIndex = 33;
+            this.label13.Text = "Danh sách lương:";
+            // 
+            // MaLuong
+            // 
+            this.MaLuong.HeaderText = "MaLuong";
+            this.MaLuong.MinimumWidth = 25;
+            this.MaLuong.Name = "MaLuong";
+            this.MaLuong.ReadOnly = true;
+            this.MaLuong.Visible = false;
+            this.MaLuong.Width = 125;
+            // 
+            // NgayCong
+            // 
+            this.NgayCong.HeaderText = "NgayCong";
+            this.NgayCong.MinimumWidth = 25;
+            this.NgayCong.Name = "NgayCong";
+            this.NgayCong.ReadOnly = true;
+            this.NgayCong.Visible = false;
+            this.NgayCong.Width = 125;
+            // 
+            // LuongCB
+            // 
+            this.LuongCB.HeaderText = "LuongCB";
+            this.LuongCB.MinimumWidth = 6;
+            this.LuongCB.Name = "LuongCB";
+            this.LuongCB.ReadOnly = true;
+            this.LuongCB.Visible = false;
+            this.LuongCB.Width = 125;
+            // 
+            // gBoxDelete
+            // 
+            this.gBoxDelete.BackColor = System.Drawing.Color.White;
+            this.gBoxDelete.Controls.Add(this.buttonEdit);
+            this.gBoxDelete.Controls.Add(this.labelEdit);
+            this.gBoxDelete.Controls.Add(this.tbDayEdit);
+            this.gBoxDelete.Controls.Add(this.label4);
+            this.gBoxDelete.Controls.Add(this.buttonDelete);
+            this.gBoxDelete.Controls.Add(this.cbbCateEdit);
+            this.gBoxDelete.Controls.Add(this.tbWageEdit);
+            this.gBoxDelete.Controls.Add(this.label5);
+            this.gBoxDelete.Controls.Add(this.label8);
+            this.gBoxDelete.Location = new System.Drawing.Point(680, 14);
+            this.gBoxDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gBoxDelete.Name = "gBoxDelete";
+            this.gBoxDelete.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gBoxDelete.Size = new System.Drawing.Size(634, 340);
+            this.gBoxDelete.TabIndex = 35;
+            this.gBoxDelete.TabStop = false;
+            // 
+            // labelEdit
+            // 
+            this.labelEdit.AutoSize = true;
+            this.labelEdit.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold);
+            this.labelEdit.Location = new System.Drawing.Point(86, 20);
+            this.labelEdit.Name = "labelEdit";
+            this.labelEdit.Size = new System.Drawing.Size(468, 41);
+            this.labelEdit.TabIndex = 21;
+            this.labelEdit.Text = "Chỉnh sửa và Xóa lương";
+            this.labelEdit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbDayEdit
+            // 
+            this.tbDayEdit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDayEdit.Location = new System.Drawing.Point(274, 153);
+            this.tbDayEdit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbDayEdit.Multiline = true;
+            this.tbDayEdit.Name = "tbDayEdit";
+            this.tbDayEdit.Size = new System.Drawing.Size(329, 36);
+            this.tbDayEdit.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(23, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 32);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Ngày công:";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonDelete.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.ForeColor = System.Drawing.Color.White;
+            this.buttonDelete.Location = new System.Drawing.Point(365, 274);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(174, 48);
+            this.buttonDelete.TabIndex = 18;
+            this.buttonDelete.Text = "Xóa";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // cbbCateEdit
+            // 
+            this.cbbCateEdit.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbCateEdit.FormattingEnabled = true;
+            this.cbbCateEdit.Items.AddRange(new object[] {
+            "Hop dong lao dong",
+            "Hop dong CTV"});
+            this.cbbCateEdit.Location = new System.Drawing.Point(274, 101);
+            this.cbbCateEdit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbbCateEdit.Name = "cbbCateEdit";
+            this.cbbCateEdit.Size = new System.Drawing.Size(329, 35);
+            this.cbbCateEdit.TabIndex = 14;
+            // 
+            // tbWageEdit
+            // 
+            this.tbWageEdit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbWageEdit.Location = new System.Drawing.Point(274, 204);
+            this.tbWageEdit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbWageEdit.Multiline = true;
+            this.tbWageEdit.Name = "tbWageEdit";
+            this.tbWageEdit.Size = new System.Drawing.Size(329, 36);
+            this.tbWageEdit.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(23, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(229, 32);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Lương cơ bản:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(23, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(191, 32);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Loại HDLD: ";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.BackColor = System.Drawing.Color.ForestGreen;
+            this.buttonEdit.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEdit.ForeColor = System.Drawing.Color.White;
+            this.buttonEdit.Location = new System.Drawing.Point(128, 274);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(174, 48);
+            this.buttonEdit.TabIndex = 22;
+            this.buttonEdit.Text = "Chỉnh sửa";
+            this.buttonEdit.UseVisualStyleBackColor = false;
+            // 
+            // uctLuong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gBoxDelete);
+            this.Controls.Add(this.dataGridViewWage);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.gboxAdd);
+            this.Name = "uctLuong";
+            this.Size = new System.Drawing.Size(1341, 565);
+            this.Load += new System.EventHandler(this.uctLuong_Load);
+            this.gboxAdd.ResumeLayout(false);
+            this.gboxAdd.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWage)).EndInit();
+            this.gBoxDelete.ResumeLayout(false);
+            this.gBoxDelete.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gboxAdd;
+        private System.Windows.Forms.Label labelAdd;
+        private System.Windows.Forms.TextBox tbDayAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ComboBox cbbCateAdd;
+        private System.Windows.Forms.TextBox tbWageAdd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridViewWage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaLuong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgayCong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LuongCB;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.GroupBox gBoxDelete;
+        private System.Windows.Forms.Label labelEdit;
+        private System.Windows.Forms.TextBox tbDayEdit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ComboBox cbbCateEdit;
+        private System.Windows.Forms.TextBox tbWageEdit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonEdit;
     }
 }
--- QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/Views/uctLuong.cs
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/Views/uctLuong.cs
@@ -8,15 +8,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using QuanLyNhanSu.Models;
+using System.Data.SqlClient;
 
 namespace QuanLyNhanSu.Views
 {
     public partial class uctLuong : DevExpress.XtraEditors.XtraUserControl
     {
+        AccessDataBase nhanvien = new AccessDataBase();
+        DataTable dtLuong = new DataTable();
+        
         public static uctLuong luong = new uctLuong();
         public uctLuong()
         {
             InitializeComponent();
+        }
+
+        private void uctLuong_Load(object sender, EventArgs e)
+        {
+            //Lấy danh sách thông tin lương vào bảng bên cạnh
+            SqlDataAdapter da = new SqlDataAdapter("select top 1000 MaLuong, NgayCong, LuongCB from Luong", AccessDataBase.connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridViewWage.DataSource = dt;
         }
     }
 }
